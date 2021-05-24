97054a08dc4b38890c61472bb78cbbc5b6b8bd05
mohammadwalikhan <mohammadwalikhan1@github.com>
2021-05-24 23:42:00 +0500

    completed assignment only 4 question left

---
+++ app.js
@@ -0,0 +1,125 @@
+// task 1 //
+// var firstName = prompt("enter your first name");
+// var lastName = prompt("enter your last name");
+// var fullName = firstName + " " + lastName;
+// document.write("welcome" + " " + fullName);
+
+// task 2 //
+
+// var favrtMbl = prompt("Enter your favourite mobile name")
+// document.write(favrtMbl.length);
+
+// Task 3 //
+
+// let a = "pakistan"
+
+// let n = str.indexOf("n",7)
+
+// Qustion 4
+
+// let str= "Hello World"
+// let lastWord=str.lastIndexOf("l")
+// document.write(lastWord)
+
+// Qustion 5 
+
+// var str = "pakistan";
+// var n = str.indexOf("i", 3);
+// document.write(n)
+
+
+// Qustion 6
+
+// let name1 = prompt("input name ");
+
+// let LastName = prompt("input last name");
+
+// let fullName = name1.concat(LastName,);
+
+// document.write(fullName);
+
+
+// Qustion 7
+
+
+// let str = 'Hyderabad';
+
+// let p = prompt("replace word")
+
+// let re = str.replace("Hyderabad",p)
+
+// document.write(re)
+
+
+// Qustion 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together"
+
+// var re = message.replace(/and/g,"&")
+
+// document.write(re);
+
+
+// Qustion 9
+
+
+// let p = prompt("input number ")
+
+// if (p * 1){
+//     document.write(p)
+// }
+// else{
+//     document.write("is not a number")
+// }
+
+
+// Qustion 10
+
+// let n = prompt (" input your name ");
+// console.log(n.toUpperCase())
+
+
+// Qustion 11
+
+// let n = prompt (" input your name ");
+// console.log(n.toTitleCase())
+
+
+// Qustion 12
+
+// var p = prompt("input number ") ;
+
+// var n = p.toString();
+
+// var re= n.replace(/./g," ")
+
+// console.log(n);
+
+
+// Qustion 13
+
+// let userName = prompt("input user name ");
+
+// if(userName=="!","@","#","$","%","^","&","*","(",")","+","=","-","[","]"){
+
+//     console.log('invled user name ')
+
+// }else{
+//     console.log(userName)
+// }
+
+
+// Qustion 14
+
+// var p = prompt("welcom to time baker what do you want sir ");
+
+// var A = ['cake', 'apple pie', ' cookie', 'chips', 'patties'];
+
+// if (p !== "A") {
+
+//     document.write("Hy This is Available In our bakery")
+// }
+// else{
+//     console.log("not avibel")
+// }
+
